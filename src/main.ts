5408ecd31b18e8c27db9910b328839a462bf3830
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2025-03-12 12:51:41 +0600

    package update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
 import {NestFactory} from '@nestjs/core';
-import {rateLimit} from 'express-rate-limit';
 import {AppModule} from './app.module';
 import {ValidationPipe} from '@nestjs/common';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
-
-const limiter = rateLimit({
-  //from per ip we allow max 5/min
-  windowMs: 1000 * 60, // 1 minutes
-  limit: 50, // Maximum 50 requests per IP in 1 minutes
-  message: 'Too many requests, please try again later.'
-});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {abortOnError: false});
@@ -32,10 +23,6 @@ async function bootstrap() {
     })
   );
 
-  //Swagger configuration
-  const config = new DocumentBuilder().setTitle('Authentication Boilerplate').setDescription('').setVersion('1.0').addTag('').build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
