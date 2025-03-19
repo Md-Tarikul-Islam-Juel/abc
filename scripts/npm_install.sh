46c8c350ae27e4ad5ae273c39474d6531fc17144
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2025-03-19 15:01:39 +0600

    update

---
+++ scripts/npm_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ex
+cd /home/ubuntu/app
+npm install
--- src/xyz/xyz.controller.ts
+++ src/xyz/xyz.controller.ts
@@ -5,6 +5,6 @@ export class XyzController {
 
     @Get()
     getHello(): string {
-        return 'hello world 24';
+        return 'hello world 25';
     }
 }
