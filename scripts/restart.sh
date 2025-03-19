232f1467ddfb12553a2143b2ad6b74f39b69aaf8
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2025-03-19 19:01:43 +0600

    update

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,3 +1,29 @@
+##!/bin/bash
+#set -ex
+#
+#export HOME=/home/ubuntu
+#export NODE_ENV=production
+#
+#echo "Navigating to app directory..."
+#cd /home/ubuntu/app
+#
+#echo "Installing dependencies..."
+#npm install --production
+#
+#echo "Stopping existing PM2 process..."
+#pm2 stop nestjs-app || true
+#pm2 delete nestjs-app || true
+#
+#echo "Starting application..."
+#pm2 start dist/main.js --name nestjs-app --watch
+#
+#echo "Saving PM2 configuration..."
+#pm2 save
+#
+#echo "Setting up startup script..."
+#pm2 startup systemd -u ubuntu --hp /home/ubuntu
+
+
 #!/bin/bash
 set -ex
 
@@ -6,6 +32,12 @@ export NODE_ENV=production
 
 echo "Navigating to app directory..."
 cd /home/ubuntu/app
+
+echo "Downloading latest artifact from S3..."
+aws s3 cp s3://seenyor-backend-1-pipeline-bucket/build_output/build_output.zip .
+
+echo "Unpacking artifact..."
+unzip -o build_output.zip -d /home/ubuntu/app
 
 echo "Installing dependencies..."
 npm install --production
@@ -20,5 +52,5 @@ pm2 start dist/main.js --name nestjs-app --watch
 echo "Saving PM2 configuration..."
 pm2 save
 
-echo "Setting up startup script..."
+echo "Configuring PM2 to start on boot..."
 pm2 startup systemd -u ubuntu --hp /home/ubuntu
--- src/xyz/xyz.controller.ts
+++ src/xyz/xyz.controller.ts
@@ -5,6 +5,6 @@ export class XyzController {
 
     @Get()
     getHello(): string {
-        return 'hello world 41';
+        return 'hello world 42';
     }
 }
