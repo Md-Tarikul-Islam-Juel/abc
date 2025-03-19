928a7cef2133451106c9fb858946753e31846704
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2025-03-19 18:31:50 +0600

    update

---
+++ scripts/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ex
+echo "Cleaning up /home/ubuntu/app..."
+rm -rf /home/ubuntu/app/*
--- src/xyz/xyz.controller.ts
+++ src/xyz/xyz.controller.ts
@@ -5,6 +5,6 @@ export class XyzController {
 
     @Get()
     getHello(): string {
-        return 'hello world 36';
+        return 'hello world 37';
     }
 }
